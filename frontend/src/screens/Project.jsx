5ca81cb1e4cd866c440dc5aa8710c70220c8251e
Kirtan <kirtanmachhi3@gmail.com>
2025-04-04 21:28:51 +0530

    og code

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -5,7 +5,7 @@ import axios from '../config/axios'
 import { initializeSocket, receiveMessage, sendMessage } from '../config/socket'
 import Markdown from 'markdown-to-jsx'
 import hljs from 'highlight.js';
-import { getWebContainer } from '../config/webContainer'
+import { getWebContainer } from '../config/webcontainer'
 
 
 function SyntaxHighlightedCode(props) {
@@ -300,40 +300,40 @@ const Project = () => {
                         <div className="actions flex gap-2">
                             <button
                                 onClick={async () => {
-                                    if (!webContainer) {
-                                        console.warn('WebContainer is not initialized yet')
-                                        return
-                                    }
-                                
                                     await webContainer.mount(fileTree)
-                                
+
+
                                     const installProcess = await webContainer.spawn("npm", [ "install" ])
-                                
+
+
+
                                     installProcess.output.pipeTo(new WritableStream({
                                         write(chunk) {
                                             console.log(chunk)
                                         }
                                     }))
-                                
+
                                     if (runProcess) {
                                         runProcess.kill()
                                     }
-                                
-                                    let tempRunProcess = await webContainer.spawn("npm", [ "start" ])
-                                
+
+                                    let tempRunProcess = await webContainer.spawn("npm", [ "start" ]);
+
                                     tempRunProcess.output.pipeTo(new WritableStream({
                                         write(chunk) {
                                             console.log(chunk)
                                         }
                                     }))
-                                
+
                                     setRunProcess(tempRunProcess)
-                                
+
                                     webContainer.on('server-ready', (port, url) => {
                                         console.log(port, url)
                                         setIframeUrl(url)
                                     })
+
                                 }}
+                                className='p-2 px-4 bg-slate-300 text-white'
                             >
                                 run
                             </button>
